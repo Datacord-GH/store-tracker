bacaf74ce04f800d1a1a5b50366f40da0fdda959
y3ll0w <46920058+y3ll0wlife@users.noreply.github.com>
2023-02-03 22:15:16 +0100

    version 1.0

--- src/models.rs
+++ src/models.rs
@@ -1,12 +1,32 @@
+#[allow(dead_code)]
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq)]
+pub struct ProductDatabase {
+    pub id: usize,
+    pub title: String,
+    pub variant_title: String,
+    pub available: bool,
+    pub price: String,
+    pub from_db: bool,
+}
+
+#[derive(Debug, Clone)]
+pub enum ProductWasChanged {
+    PriceChange,
+    NoLongerAvailable,
+    IsAvailable,
+    VariantTitle,
+    ProductTitle,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ProductRespose {
     pub products: Vec<Product>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Product {
     pub id: usize,
     pub title: String,
@@ -18,13 +38,13 @@ pub struct Product {
     pub vendor: String,
     pub product_type: String,
     pub tags: Vec<String>,
-    pub variants: Vec<ProductVariants>,
+    pub variants: Vec<ProductVariant>,
     pub images: Vec<ProductImage>,
     pub options: Vec<ProductOption>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ProductVariants {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ProductVariant {
     pub id: usize,
     pub title: String,
     pub option1: Option<String>,
@@ -44,7 +64,7 @@ pub struct ProductVariants {
     pub updated_at: DateTime<Utc>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ProductImage {
     pub id: usize,
     pub product_id: usize,
@@ -58,7 +78,7 @@ pub struct ProductImage {
     pub variant_ids: Vec<usize>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ProductOption {
     pub name: String,
     pub position: usize,
